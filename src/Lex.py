29f853c5f7e56c3a4757f0eec9c637d879ce4e06
joycxmac <155485833+joycxmac@users.noreply.github.com>
2026-04-20 13:08:48 +0200

    adding LPAREN and RPAREN

--- src/Lex.py
+++ src/Lex.py
@@ -1,15 +1,27 @@
 
+from logic_ast import LPAREN, RPAREN
+
+
+def t_VAR(t):
+    r'[A-Z][A-Z0-9_]*'
+    return t
+
 tokens = (
+    VAR,
     NEG,    # (¬)
     CONJ,   # (∧)
     DISJ,   # (∨)
     IMPL,   # (→)
     BICOND, # (↔)
+    LPAREN, # (()
+    RPAREN, # ())
 )
 
-t_NEG   = r'!='
+t_NEG   = r'!'
 t_CONJ   = r'&'
-t_DISJ   = r'||'
+t_DISJ   = r'\|\|'
 t_IMPL   = r'->'
 t_BICOND = r'<->'
+t_LPARENT = r'\('
+t_RPARENT = r'\)'
 
